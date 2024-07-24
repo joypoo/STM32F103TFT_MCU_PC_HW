34a554a5943eed1df8e755624f8560ca32c82c85
joy9l- <joy9l-@noreply.gitcode.com>
2024-07-24 12:57:46 +0800

    all

---
+++ CommMFC/resource.h
@@ -0,0 +1,51 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 CommMFC.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_COMMMFC_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDB_PNG1                        132
+#define IDB_BITMAP1                     133
+#define IDC_BUTTON_OPEN_CLOSE           1000
+#define IDC_COMBO_PORT_Nr               1001
+#define IDC_COMBO_BAUDRATE              1002
+#define IDC_EDIT1                       1003
+#define IDC_ReceiveEdit                 1003
+#define IDC_SendEdit                    1004
+#define IDC_BUTTON_SEND                 1005
+#define IDC_BUTTON_TIMER                1006
+#define IDC_STATIC_RECV_COUNT           1006
+#define IDC_STATIC_RECV_COUNT_VALUE     1007
+#define IDC_STATIC_SEND_COUNT           1008
+#define IDC_STATIC_SEND_COUNT_VALUE     1009
+#define IDC_BUTTON_CLEAR                1010
+#define IDC_COMBO_PARITY                1011
+#define IDC_COMBO_STOP                  1012
+#define IDC_COMBO_DATABITS              1013
+#define IDC_EDIT_RECEIVE_TIMEOUT_MS     1015
+#define IDC_STATIC_RECEVICE_TIMEOUT_MS  1016
+#define IDC_CHECK_DTR                   1017
+#define IDC_CHECK_RTS                   1018
+#define IDC_BUTTON1                     1019
+#define IDC_BUTTON_IMPORT               1019
+#define IDC_COMBO1                      1020
+#define IDC_COMBO_PIC_NO                1020
+#define IDC_BUTTON2                     1021
+#define ID_PIC_1                        1022
+#define IDC_PIC1                        1023
+#define IDC_EDIT2                       1024
+#define IDC_EDIT_IMPORTFILE             1024
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1025
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
